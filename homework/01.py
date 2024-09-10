4f855eb8bdddfef36680c0f24c5e9275b02d89dc
o5xNo <asd38894268@gmail.com>
2024-09-10 11:42:53 +0800

    re

---
+++ homework/01.py
@@ -0,0 +1,14 @@
+def power(n):
+    dp=[0]*(n+1)
+    dp[0]=1
+
+    for i in range(1,n+1):
+        dp[i]=dp[i-1]+dp[i-1]
+    return dp[n]
+
+def main():
+    n=int(input())
+    print(f"2^{n} = {power(n)}")
+
+if __name__ == "__main__":
+    main()
